50a5adc8490a017ff75a1066cd181e8d1d108290
MHMD9Khatib <mmkhatib99@gmail.com>
2022-10-26 12:06:12 +0300

    Heroku test 3

--- src/connection/db.js
+++ src/connection/db.js
@@ -2,12 +2,16 @@ const Pool = require('pg').Pool
 require("env2")(".env");
 
 const pool = new Pool({
-  
-  user: 'me',
-  host: 'localhost',
-  database: 'booklist',
-  password: 'password',
-  port: 5432,
+  // user: 'me',
+  // host: 'localhost',
+  // database: 'booklist',
+  // password: 'password',
+  // port: 5432,
+  user : process.env.User,
+  host: process.env.Host,
+  database: process.env.Database,
+  password: process.env.Password,
+  port: process.env.Port,
 })
 
 
